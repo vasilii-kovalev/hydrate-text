c1ae6709ec1fae7466a4d0ed3f323717f5b3a052
Vasilii Kovalev <vasya.kowalev@yandex.ru>
2020-07-25 11:35:41 +0300

    Make CJS and UMD builds
    * Add Webpack and write a config to create UMD builds
    * Make Webpack building normal and minified UMD builds
    * Add TS configs for CJS (tsconfig.node.json) and UMD (tsconfig.webpack.json) builds
    * Write some scripts to create different builds
    * Update .eslintrc and up its version
    * Up library version to 1.2.0

---
+++ webpack.config.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+
+import TerserPlugin from 'terser-webpack-plugin';
+import { ConfigurationFactory } from 'webpack';
+
+const configurationFactory: ConfigurationFactory = env => {
+  const parsedMinimize = typeof env === 'string'
+    ? false
+    : env?.MINIMIZE;
+  const minimize = Boolean(parsedMinimize);
+
+  return {
+    mode: 'production',
+    entry: {
+      index: path.resolve(__dirname, './dist/esm/index.js'),
+    },
+    output: {
+      path: path.resolve(__dirname, './dist/umd'),
+      filename: `[name]${minimize ? '.min' : ''}.js`,
+      library: 'hydrateText',
+      libraryTarget: 'umd',
+      globalObject: 'this',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.t|js$/,
+          use: 'babel-loader',
+        },
+      ],
+    },
+    optimization: {
+      minimize,
+      minimizer: [
+        new TerserPlugin(),
+      ],
+    },
+  };
+};
+
+module.exports = configurationFactory;
